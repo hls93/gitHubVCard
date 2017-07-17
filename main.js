326419ede31ca6ea5fcc0d23ec4b8a663049a41f
hls93 <hlshaw3@ncsu.edu>
2017-07-17 18:32:44 -0400

    making changes to css and javascript

---
+++ main.js
@@ -0,0 +1,38 @@
+//name, Github Url, Email, Company, Website
+console.log("hello world");
+let picture = document.querySelector('.picture')
+// want to pull a new XMLHttpRequest
+let request = new XMLHttpRequest();
+request.addEventListener("load", displayCharacters);
+request.open('GET', 'https://api.github.com/users/hls93');
+request.send();
+
+// console.log(request);
+
+function displayCharacters() {
+  let data = JSON.parse(this.responseText);
+  let list = '';
+  console.log(data);
+  picture.innerHTML = `<header>
+          <h1>Helen Shaw</h1>
+        </header>
+        <div class="bottom">
+          <div class="left">
+            <h3>The Basics</h3>
+            <ul class="myList">
+              <li><span>name:</span> ${data.name}</li>
+              <li><span>Github Url:</span> ${data.login}</li>
+              <li><span>email:</span> ${data.email}</li>
+              <li><span>company:</span> ${data.company}</li>
+              <li><span>website:</span> ${data.type}
+              </ul>
+            </div>
+              <div class="right">
+                <h3>The Story</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                <div class="image">
+                <img src="${data.avatar_url}" alt="">
+                </div>
+              </div>`;
+}
